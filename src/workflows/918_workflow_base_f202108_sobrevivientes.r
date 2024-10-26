ac202f09b7be0066d589660567fdcb6686047797
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2024-10-26 18:13:53 -0300

    Update and rename 918_workflow_base_f202108.r to 918_workflow_base_f202108_sobrevivientes.r

--- src/workflows/918_workflow_base_f202108_sobrevivientes.r
+++ src/workflows/918_workflow_base_f202108_sobrevivientes.r
@@ -420,7 +420,7 @@ KA_evaluate_kaggle <- function( pinputexps )
 # Este es el  Workflow Baseline
 # Que predice 202108 donde NO conozco la clase
 
-wf_agosto_gf_AAAA_MM_DD <- function( pnombrewf )
+wf_agosto_gf_sobrevivientes <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
@@ -429,11 +429,11 @@ wf_agosto_gf_AAAA_MM_DD <- function( pnombrewf )
 
   # Etapas preprocesamiento
   CA_catastrophe_base( metodo="MachineLearning")
-  FEintra_manual_base()
-  DR_drifting_base(metodo="rank_cero_fijo")
-  FEhist_base()
+  #FEintra_manual_base()
+  #DR_drifting_base(metodo="rank_cero_fijo")
+  #FEhist_base()
 
-  FErf_attributes_base( arbolitos= 20,
+  #FErf_attributes_base( arbolitos= 20,
     hojas_por_arbol= 16,
     datos_por_hoja= 1000,
     mtry_ratio= 0.2
