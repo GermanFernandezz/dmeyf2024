ff6e01ebe035a9bd134398a4884eeb7e4448e41f
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2024-11-01 17:43:01 -0300

    Update variables_evolutivas.r

--- src/variables_evolutivas.r
+++ src/variables_evolutivas.r
@@ -2,14 +2,14 @@ require( "data.table" )
 
 #Especificar carpeta donde guarda el dataset los canarios asesinos.
 #Si va a ser la primer iteracion, especificar carpeta del dataset original
-setwd("~/buckets/b1/datasets") # Establezco el Working Directory
+setwd("~/buckets/b1/expw/CN-0006") # Establezco el Working Directory
 
 #Nombre del dataset
-dataset <- fread("competencia_02.csv.gz")
+dataset <- fread("dataset.csv.gz")
 
 #Nombre de la carpeta donde se guardan la importancia de las variables. suele ser la del ultimo canario asesino
 #Si es primer iteración, correr un canario asesino que da el archivo con la importancia
-setwd("~/buckets/b1/expw/CN-0005")
+setwd("~/buckets/b1/expw/CN-0006")
 
 #Nombre del archivo con las variables ordendas por importancia.
 impo_1 <- fread("impo_1.txt")
