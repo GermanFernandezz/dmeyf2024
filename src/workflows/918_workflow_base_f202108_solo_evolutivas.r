34035d313205052d67982681b6c41fd18db845f6
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2024-11-03 09:20:08 -0300

    Update 918_workflow_base_f202108_solo_evolutivas.r

--- src/workflows/918_workflow_base_f202108_solo_evolutivas.r
+++ src/workflows/918_workflow_base_f202108_solo_evolutivas.r
@@ -440,7 +440,7 @@ wf_agosto_base_canaritos <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/dataset_iter_2.csv.gz")
+  DT_incorporar_dataset( "~/buckets/b1/expw/dataset.csv.gz")
 
   # Etapas preprocesamiento
   FEintra_manual_evolutivas()
