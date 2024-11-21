b30443990f950bd3974cc8abbcff5dcc88c78713
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2024-11-21 10:23:05 -0300

    Add files via upload

--- src/traduccion_variables_importantes.R
+++ src/traduccion_variables_importantes.R
@@ -1,11 +1,20 @@
 
-
+# Queda fijo: es el archivo de todas las variables nuevas traducidas en función de las
+# variables originales
 data_final <- read.csv("variables_traducidas.txt")
-impo_1 <- read.table("expw_CN-0015_impo_1.txt", header = TRUE)
+
+
+
+for (k in 17:20) {
+# Es el archivo de las variables ordenadas por importancia que sale de canaritos
+
+  archivo <- paste0("expw_CN-00",k,"_impo_1.txt")
+  impo_1 <- read.table(archivo, header = TRUE)
 
 impo_1$explicacion_final <- NA
 
-# Iterar sobre cada valor de var1 en data2
+# La posta: es el que traduce las variables. Las 20 mas importantes de acá son las que van
+# a servir para hacer las próxima generación de variables
 for (i in 1:nrow(impo_1)) {
   # Buscar la coincidencia del valor de var1 en el nombre de data1
   match_index <- match(impo_1$Feature[i], data_final$nombre)
@@ -16,5 +25,8 @@ for (i in 1:nrow(impo_1)) {
   }
 }
 
-
-write.table(impo_1, "impo_1_variables_explicadas.txt", sep = ",", row.names = FALSE, col.names = TRUE)
+# Guarda en un txt los resultados.
+# HAY QUE CAMBIAR iter_n con n=iteración.
+archivo_impo <- paste0("traduccion",archivo)
+write.table(impo_1, archivo_impo, sep = ",", row.names = FALSE, col.names = TRUE)
+}
